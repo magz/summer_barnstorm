5754ecea0c9940cfdc3fa0ce27c7ddccd806b686
magz <magz@server.local>
2012-05-25 10:49:07 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SummerBarnstorm::Application.config.secret_token = 'a392b0928cea62cb9f3e0586c752525eff2d3776c9ec41f5e20566f464ad472fab6293fa841c047c476fc7b1d7847b7f489e0cbca050970c32042c687270dce1'
