0900859cac430d12487e685eb62dda23ba11bc50
magz <magz@server.local>
2012-05-29 11:23:13 -0400

    remmoving done boolean from team data for the moment

--- db/migrate/20120529150809_add_datato_teams.rb
+++ db/migrate/20120529150809_add_datato_teams.rb
@@ -3,7 +3,6 @@ class AddDatatoTeams < ActiveRecord::Migration
   	change_table :team_data do |t|
   		t.string :city
   		t.string :team_name
-  		t.boolean :done, :default => false
   	end
 
   end
@@ -11,6 +10,5 @@ class AddDatatoTeams < ActiveRecord::Migration
   def down
   	remove_column :team_data, :city
   	remove_column :team_data, :team_name
-  	remove_column :team_data, :done
   end
 end
