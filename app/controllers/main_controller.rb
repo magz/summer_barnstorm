0570985b01beab02d48a84c990c7e28ac58c17b9
magz <magz@server.local>
2012-06-07 15:21:06 -0400

    sms modal partially working

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -19,12 +19,20 @@ class MainController < ApplicationController
   		redirect_to "http://click.linksynergy.com/fs-bin/stat?id=HXDYbs8K8P4&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1=http%253A%252F%252Fitunes.apple.com%252Fus%252Fapp%252Ftopps-pennant%252Fid514201097%253Fmt%253D8%2526uo%253D4%2526partnerId%253D30"
   		#redirect_to "http://google.com"
   	else
-  		redirect_to :coming_soon
+  		redirect_to :welcome
   	end
   end
 
   def send_sms
+  	target_number = params[:phone_number]
+  	unless target_number[0] == "1"
+  		target_number = "1" + target_number
+  	end
 
+  	sms = Moonshado::Sms.new(target_number, "this is where the link and such would be")
+  	sms.deliver_sms
+  	
+  	render json: "success!"
   	
   end
 end
