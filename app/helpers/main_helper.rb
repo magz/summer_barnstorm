49140318b72b38f926c2eeba09d5fa2e30ef77f3
magz <magz@server.local>
2012-06-12 17:17:55 -0400

    changing map points to floats

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,6 +1,11 @@
 module MainHelper
 
+MAP_RATIO = 840.0 / 1491.0
+
 def disc(team, size = "large", color=true)
+
+		# This represents the ratio between the original map image size (1491) and the current setting (840)
+		# It is currently == 0.563380282
 
 		image_path = "large_logo_icons/"
 
@@ -12,13 +17,13 @@ def disc(team, size = "large", color=true)
 
 		image_path += team.team_name.downcase
 		if size == "small"
-			image_size = "75x75"
+			side = "50"
 		else
-			image_size = "150x150"
+			side = "150"
 		end
-		style = "left: #{team.x_map_point}px; bottom: #{team.y_map_point}px"
+		style = "left: #{team.x_map_point.to_f * MAP_RATIO - side.to_f/2}px ; top: #{team.y_map_point.to_f * MAP_RATIO - side.to_f/2}px"
 		
-		image_tag image_path + ".png", :class => "#{size}_disc", :style => style, :size => image_size
+		image_tag image_path + ".png", :class => "#{size}_disc", :style => style, :size => side + "x" + side
 
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,37 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120601193400) do
+ActiveRecord::Schema.define(:version => 20120612211019) do
+
+  create_table "team_data", :force => true do |t|
+    t.string   "color"
+    t.string   "small_image"
+    t.integer  "x_map_point"
+    t.integer  "y_map_point"
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
+    t.string   "city"
+    t.string   "team_name"
+    t.boolean  "done",        :default => false
+  end
 
   create_table "teams", :force => true do |t|
     t.string   "color"
-    t.integer  "x_map_point"
-    t.integer  "y_map_point"
+    t.float    "x_map_point"
+    t.float    "y_map_point"
     t.string   "city"
     t.string   "team_name"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
     t.string   "twitter_tags"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "role"
   end
 
 end
