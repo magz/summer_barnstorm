838ef3c6b0e4dd5c411c2c8326c1a8606bb8e5e6
magz <magz@server.local>
2012-06-14 14:50:38 -0400

    separated logos and discs, they now move independently and are containted ina  div

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -2,30 +2,74 @@ module MainHelper
 
 MAP_RATIO = 840.0 / 1491.0
 
-def disc(team, size = "large", color=true)
+	def blank_disc(team, color=true)
 
-		# This represents the ratio between the original map image size (1491) and the current setting (840)
-		# It is currently == 0.563380282
+			# This represents the ratio between the original map image size (1491) and the current setting (840)
+			# It is currently == 0.563380282
 
-		image_path = "large_logo_icons/"
+			image_path = "blank_discs/"
+
+			if color
+				image_path += team.team_name + "_blank_color_disc"
+			else
+				image_path += "grey_blank_disc"
+			end
+
+			image_path += ".png"
+
+			style = team_center_css(team, side)
+			image_tag image_path, :class => "team_disc", :data => {team: team.team_name.downcase}, :style => style, :size => side.to_s + "x" + side.to_s
+
+	end
+
+	def team_logo(team)
+		image_path = "team_logos/" + team.team_name + "_logo.png"
+
+		side = 22
+		style = team_center_css(team, 44)
+
+
+		image_tag image_path, :class => "team_logo", :data => {team: team.team_name.downcase}, :style => style, :size => side.to_s + "x" + side.to_s
+
+
+
+
+	end
+
+	# def team_center_css(team, side_size)
+	# 	"left: #{team.x_map_point.to_f * MAP_RATIO - side_size.to_f/2}px ; top: #{team.y_map_point.to_f * MAP_RATIO - side_size.to_f/2}px"
+	# end
+
+	def get_center_css(team, side_size)
+		"left: #{team.x_map_point.to_f * MAP_RATIO - side_size.to_f/2}px ; top: #{team.y_map_point.to_f * MAP_RATIO - side_size.to_f/2}px"
+	end
+
+	def small_team_disc_generator(team, color = true)
+		center_css = get_center_css(team, 45)
+		("<div class=small_team_disc style='" + center_css + "'>" + new_blank_disc(team, color) + new_team_logo(team) + "</div>").html_safe 
+
+	end
+
+	def new_blank_disc(team, color=true)
+		image_path = "blank_discs/"
 
 		if color
-			image_path += "color_disc_" 
+			image_path += team.team_name + "_blank_color_disc"
 		else
-			image_path += "grey_disc_"
+			image_path += "grey_blank_disc"
 		end
 
-		image_path += team.team_name.downcase
-		if size == "small"
-			side = "40"
-		else
-			side = "150"
-		end
-		style = "left: #{team.x_map_point.to_f * MAP_RATIO - side.to_f/2}px ; top: #{team.y_map_point.to_f * MAP_RATIO - side.to_f/2}px"
-		
-		image_tag image_path + ".png", :class => "team_disc", :style => style, :size => side + "x" + side
+		image_path += ".png"
 
-end
+		image_tag image_path, :class => "blank_disc"
 
+
+	end
+
+	def new_team_logo(team)
+		image_path = "team_logos/" + team.team_name + "_logo.png"
+		image_tag image_path, :class => "team_logo"
+
+	end
 
 end
