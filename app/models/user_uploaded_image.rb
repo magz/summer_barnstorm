4e79141523e649dfdea1edcdab505d04f4a977ed
magz <magz@server.local>
2012-06-20 16:27:53 -0400

    still setting up paperclip/s3 for heroku

--- app/models/user_uploaded_image.rb
+++ app/models/user_uploaded_image.rb
@@ -1,10 +1,12 @@
 class UserUploadedImage < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :team
-  has_attached_file :screenshot, 
-  	:storage => :s3, :s3_credentials => "#{Rails.root}/config/amazon_s3.yml", 
-  	:path => "user_uploaded_images/:attachment/:style/:id.:extension",
-  	:bucket => "pennant_images"
-
+  has_attached_file :screenshot,
+    :storage => :s3,
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :s3_credentials => {
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
 
 end
