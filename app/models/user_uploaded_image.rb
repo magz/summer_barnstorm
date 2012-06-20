1e6e508422478ea03d4b2bf7b3fe2ab9c31418a7
magz <magz@server.local>
2012-06-20 16:23:20 -0400

    paperclip/s3 appears to all be set up...

--- app/models/user_uploaded_image.rb
+++ app/models/user_uploaded_image.rb
@@ -1,6 +1,10 @@
 class UserUploadedImage < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :team
-  has_attached_file :screenshot
+  has_attached_file :screenshot, 
+  	:storage => :s3, :s3_credentials => "#{Rails.root}/config/amazon_s3.yml", 
+  	:path => "user_uploaded_images/:attachment/:style/:id.:extension",
+  	:bucket => "pennant_images"
+
 
 end
