469c3f3d08b72dba888ac12b6b8350bc6b3e72a2
magz <magz@server.local>
2012-07-24 15:22:01 -0400

    cards for current red sox royals promo

--- app/helpers/main_controller_helpers.rb
+++ app/helpers/main_controller_helpers.rb
@@ -15,9 +15,9 @@ module MainControllerHelpers
 		overlay_images = []
 
 		if team == "default"
-			overlay_images = ["default_memories.jpg", "default_futurecard.jpg", 
+			overlay_images = ["default_memories.jpg", "red_sox_futurecard.jpg", 
 				"default_sharecard.jpg", "tigers_3.jpg", "braves_1.jpg", "giants_2.jpg",
-				"yankees_2.jpg", "red_sox_2.jpg", "cardinals_1.jpg", "default_titlecard.jpg"]
+				"yankees_2.jpg", "red_sox_2.jpg", "cardinals_1.jpg", "red_sox_titlecard.jpg"]
 			overlay_images.map! {|x| card_path + x}
 
 		else
