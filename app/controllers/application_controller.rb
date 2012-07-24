94f0f3ad598d8311ab3d72239fa2f039ef54ddcf
magz <magz@server.local>
2012-07-24 14:59:00 -0400

    trying to figure out full path thing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,11 @@ class ApplicationController < ActionController::Base
 	#csfr?  is there a reason that's off?  
 	before_filter :get_browser
 
+
 	def get_browser
       @browser = Browser.new(ua: request.env['HTTP_USER_AGENT'], :accept_language => "en-us")
+	  puts "full path is: " + request.fullpath 
+	  puts "http://#{request.host}:#{request.port.to_s + request.fullpath}"
 	end
 
 end
